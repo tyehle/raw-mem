596389463ef4e87b34e1acae1e8e99d234c662a0
Tobin Yehle <tobinyehle@gmail.com>
2017-11-17 21:50:57 -0700

    count some bits

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,21 @@
 module Main where
 
-import Lib
+import System.IO
+import qualified Data.ByteString as B
+import Data.Bits (popCount)
+import Data.List (foldl')
 
 main :: IO ()
-main = someFunc
+main = do
+  handle <- openFile "/dev/mem" ReadMode
+  bytes <- B.hGet handle 1000000
+  let (ones, total) = countPops bytes
+  putStrLn $ "Set bits: " ++ show ones
+  putStrLn $ "Total bits: " ++ show total
+  putStrLn $ "Ratio of set bits: " ++ show (fromIntegral ones / fromIntegral total)
+
+countPops :: B.ByteString -> (Integer, Integer)
+countPops bytes = (ones, total)
+  where
+    ones = sum . map (fromIntegral . popCount) $ B.unpack bytes
+    total = 8 * fromIntegral (B.length bytes)
